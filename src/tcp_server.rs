7a93ed352078702b495887ebefedd6f245c873ef
rotoclone <rotoclone@gmail.com>
2023-01-17 23:46:40 -0600

    Fix TCP server

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,17 +1,9 @@
-use std::{sync::Arc, time::Duration};
-
 use anyhow::Result;
 use core_logic::Game;
 use futures::{SinkExt, StreamExt};
 use log::debug;
-use std::fmt::Write;
-use tokio::{
-    io::AsyncReadExt,
-    io::AsyncWriteExt,
-    net::{TcpListener, TcpStream},
-    sync::Mutex,
-};
-use tokio_util::codec::{BytesCodec, Decoder, LinesCodec};
+use tokio::net::TcpListener;
+use tokio_util::codec::{Decoder, LinesCodec};
 
 use crate::{delay_for_message, message_to_string};
 
@@ -31,24 +23,18 @@ pub async fn start_server() -> Result<()> {
     let mut next_player_id = 0;
     loop {
         // Asynchronously wait for an inbound socket.
-        let (socket, socket_addr) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
 
         let (command_sender, message_receiver) =
             game.add_player(format!("Player {}", next_player_id));
         next_player_id += 1;
 
-        // We're parsing each socket with the `BytesCodec` included in `tokio::codec`.
-        //TODO let framed = Arc::new(Mutex::new(LinesCodec::new().framed(socket)));
-        //TODO let other_framed = Arc::clone(&framed);
-
         let (mut sink, mut stream) = LinesCodec::new().framed(socket).split::<String>();
 
-        //TODO let (read_socket, write_socket) = socket.into_split();
-
-        // spawn worker for sending messages to player
+        // spawn task for sending messages to player
         tokio::spawn(async move {
             loop {
-                let (message, game_time) = match message_receiver.recv() {
+                let (message, game_time) = match message_receiver.recv_async().await {
                     Ok(x) => x,
                     Err(_) => {
                         debug!("Message sender has been dropped");
@@ -58,17 +44,14 @@ pub async fn start_server() -> Result<()> {
                 debug!("Got message: {message:?}");
                 let delay = delay_for_message(&message);
                 let rendered_message = message_to_string(message, Some(game_time));
-                debug!("Rendered message:\n{rendered_message}");
-                /* TODO
-                sink.send(rendered_message)
+                sink.send(format!("{rendered_message}\n"))
                     .await
                     .expect("should be able to send rendered message");
-                    */
                 tokio::time::sleep(delay).await;
             }
         });
 
-        // spawn worker for receiving commands from player
+        // spawn task for receiving commands from player
         tokio::spawn(async move {
             // The stream will return None once the client disconnects.
             while let Some(message) = stream.next().await {
@@ -83,6 +66,7 @@ pub async fn start_server() -> Result<()> {
                 }
             }
             println!("Socket received FIN packet and closed connection");
+            //TODO remove player from game
         });
     }
 }
