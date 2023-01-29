f0f94e6945267e67358ceb75e19bfb90226f95f6
rotoclone <rotoclone@gmail.com>
2023-01-29 14:30:40 -0600

    Add WornItems component

---
+++ crates/core-logic/src/component/wearable.rs
@@ -0,0 +1,14 @@
+use std::collections::HashSet;
+
+use bevy_ecs::prelude::*;
+
+use crate::BodyPart;
+
+/// An entity that can be worn.
+#[derive(Component)]
+pub struct Wearable {
+    /// The thickness of the entity.
+    pub thickness: u32,
+    /// THe body parts the entity covers when worn.
+    pub body_parts: HashSet<BodyPart>,
+}
