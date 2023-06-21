4d5067b76ef4269db6980c187fdf9c76a26ccbfc
Steven Goldberg <rotoclone@gmail.com>
2023-06-21 18:16:21 -0400

    Implement unholding

--- crates/core-logic/src/action/hold.rs
+++ crates/core-logic/src/action/hold.rs
@@ -4,7 +4,8 @@ use regex::Regex;
 
 use crate::{
     component::{
-        ActionEndNotification, AfterActionPerformNotification, HeldItems, HoldError, Item, Location,
+        ActionEndNotification, AfterActionPerformNotification, HeldItems, HoldError, Item,
+        Location, UnholdError,
     },
     find_wearing_entity, get_reference_name,
     input_parser::{
@@ -22,13 +23,16 @@ use super::{
 };
 
 const HOLD_VERB_NAME: &str = "hold";
+const UNHOLD_VERB_NAME: &str = "put away";
 const HOLD_FORMAT: &str = "hold <>";
+const UNHOLD_FORMAT: &str = "put away <>";
 const NAME_CAPTURE: &str = "name";
 
 lazy_static! {
     static ref HOLD_PATTERN: Regex =
-        Regex::new("^(hold|equip|wield|take out) (the )?(?P<name>.*)").unwrap();
-    //TODO allow unholding
+        Regex::new("^(hold|equip|wield|unholster|take out) (the )?(?P<name>.*)").unwrap();
+    static ref UNHOLD_PATTERN: Regex =
+        Regex::new("^(unhold|unequip|unwield|holster|stow|put away) (the )?(?P<name>.*)").unwrap();
 }
 
 pub struct HoldParser;
@@ -40,34 +44,41 @@ impl InputParser for HoldParser {
         source_entity: Entity,
         world: &World,
     ) -> Result<Box<dyn Action>, InputParseError> {
-        if let Some(captures) = HOLD_PATTERN.captures(input) {
-            if let Some(target_match) = captures.name(NAME_CAPTURE) {
-                let target = CommandTarget::parse(target_match.as_str());
-                if let Some(target_entity) = target.find_target_entity(source_entity, world) {
-                    if world.get::<Item>(target_entity).is_some() {
-                        // target exists and is holdable
-                        return Ok(Box::new(HoldAction {
-                            target: target_entity,
-                            notification_sender: ActionNotificationSender::new(),
-                        }));
-                    } else {
-                        // target isn't holdable
-                        let target_name =
-                            get_reference_name(target_entity, Some(source_entity), world);
-                        return Err(InputParseError::CommandParseError {
-                            verb: HOLD_VERB_NAME.to_string(),
-                            error: CommandParseError::Other(format!(
-                                "You can't hold {target_name}."
-                            )),
-                        });
-                    }
+        let (captures, verb_name, should_be_held) =
+            if let Some(captures) = HOLD_PATTERN.captures(input) {
+                (captures, HOLD_VERB_NAME, true)
+            } else if let Some(captures) = UNHOLD_PATTERN.captures(input) {
+                (captures, UNHOLD_VERB_NAME, false)
+            } else {
+                return Err(InputParseError::UnknownCommand);
+            };
+
+        if let Some(target_match) = captures.name(NAME_CAPTURE) {
+            let target = CommandTarget::parse(target_match.as_str());
+            if let Some(target_entity) = target.find_target_entity(source_entity, world) {
+                if world.get::<Item>(target_entity).is_some() {
+                    // target exists and is holdable
+                    return Ok(Box::new(HoldAction {
+                        target: target_entity,
+                        should_be_held,
+                        notification_sender: ActionNotificationSender::new(),
+                    }));
                 } else {
-                    // target doesn't exist
+                    // target isn't holdable
+                    let target_name = get_reference_name(target_entity, Some(source_entity), world);
                     return Err(InputParseError::CommandParseError {
                         verb: HOLD_VERB_NAME.to_string(),
-                        error: CommandParseError::TargetNotFound(target),
+                        error: CommandParseError::Other(format!(
+                            "You can't {verb_name} {target_name}."
+                        )),
                     });
                 }
+            } else {
+                // target doesn't exist
+                return Err(InputParseError::CommandParseError {
+                    verb: verb_name.to_string(),
+                    error: CommandParseError::TargetNotFound(target),
+                });
             }
         }
 
@@ -75,17 +86,18 @@ impl InputParser for HoldParser {
     }
 
     fn get_input_formats(&self) -> Vec<String> {
-        vec![HOLD_FORMAT.to_string()]
+        vec![HOLD_FORMAT.to_string(), UNHOLD_FORMAT.to_string()]
     }
 
     fn get_input_formats_for(&self, entity: Entity, world: &World) -> Option<Vec<String>> {
-        input_formats_if_has_component::<Item>(entity, world, &[HOLD_FORMAT])
+        input_formats_if_has_component::<Item>(entity, world, &[HOLD_FORMAT, UNHOLD_FORMAT])
     }
 }
 
 #[derive(Debug)]
 pub struct HoldAction {
     pub target: Entity,
+    pub should_be_held: bool,
     pub notification_sender: ActionNotificationSender<Self>,
 }
 
@@ -94,40 +106,60 @@ impl Action for HoldAction {
         let target = self.target;
         let target_name = get_reference_name(target, Some(performing_entity), world);
 
-        match HeldItems::hold(performing_entity, target, world) {
-            Ok(()) => (),
-            Err(HoldError::CannotHold) => {
-                return ActionResult::builder()
-                    .with_error(performing_entity, "You can't hold things.".to_string())
-                    .build_complete_no_tick(false)
+        if self.should_be_held {
+            match HeldItems::hold(performing_entity, target, world) {
+                Ok(()) => (),
+                Err(HoldError::CannotHold) => {
+                    return ActionResult::builder()
+                        .with_error(performing_entity, "You can't hold things.".to_string())
+                        .build_complete_no_tick(false)
+                }
+                Err(HoldError::CannotBeHeld) => {
+                    return ActionResult::builder()
+                        .with_error(performing_entity, format!("You can't hold {target_name}."))
+                        .build_complete_no_tick(false)
+                }
+                Err(HoldError::AlreadyHeld) => {
+                    return ActionResult::builder()
+                        .with_error(
+                            performing_entity,
+                            format!("You're already holding {target_name}."),
+                        )
+                        .build_complete_no_tick(false)
+                }
+                Err(HoldError::NotEnoughHands) => {
+                    return ActionResult::builder()
+                        .with_error(
+                            performing_entity,
+                            format!("You don't have enough free hands to hold {target_name}."),
+                        )
+                        .build_complete_no_tick(false);
+                }
             }
-            Err(HoldError::CannotBeHeld) => {
-                return ActionResult::builder()
-                    .with_error(performing_entity, format!("You can't hold {target_name}."))
-                    .build_complete_no_tick(false)
-            }
-            Err(HoldError::AlreadyHeld) => {
-                return ActionResult::builder()
-                    .with_error(
-                        performing_entity,
-                        format!("You're already holding {target_name}."),
-                    )
-                    .build_complete_no_tick(false)
-            }
-            Err(HoldError::NotEnoughHands) => {
-                return ActionResult::builder()
-                    .with_error(
-                        performing_entity,
-                        format!("You don't have enough free hands to hold {target_name}."),
-                    )
-                    .build_complete_no_tick(false);
+        } else {
+            match HeldItems::unhold(performing_entity, target, world) {
+                Ok(()) => (),
+                Err(UnholdError::NotHolding) => {
+                    return ActionResult::builder()
+                        .with_error(
+                            performing_entity,
+                            format!("You're not holding {target_name}."),
+                        )
+                        .build_complete_no_tick(false)
+                }
             }
         }
+
+        let (take_out_or_put_away, takes_out_or_puts_away) = if self.should_be_held {
+            ("take out", "takes out")
+        } else {
+            ("put away", "puts away")
+        };
 
         ActionResult::builder()
             .with_message(
                 performing_entity,
-                format!("You take out {target_name}."),
+                format!("You {take_out_or_put_away} {target_name}."),
                 MessageCategory::Internal(InternalMessageCategory::Action),
                 MessageDelay::Short,
             )
@@ -139,7 +171,7 @@ impl Action for HoldAction {
                     MessageDelay::Short,
                 )
                 .add_entity_name(performing_entity)
-                .add_string(" takes out ".to_string())
+                .add_string(format!(" {takes_out_or_puts_away} "))
                 .add_entity_name(target)
                 .add_string(".".to_string()),
                 world,
@@ -148,12 +180,21 @@ impl Action for HoldAction {
     }
 
     fn interrupt(&self, performing_entity: Entity, _: &mut World) -> ActionInterruptResult {
-        ActionInterruptResult::message(
-            performing_entity,
-            "You stop holding things.".to_string(),
-            MessageCategory::Internal(InternalMessageCategory::Action),
-            MessageDelay::None,
-        )
+        if self.should_be_held {
+            ActionInterruptResult::message(
+                performing_entity,
+                "You stop holding things.".to_string(),
+                MessageCategory::Internal(InternalMessageCategory::Action),
+                MessageDelay::None,
+            )
+        } else {
+            ActionInterruptResult::message(
+                performing_entity,
+                "You stop putting things away.".to_string(),
+                MessageCategory::Internal(InternalMessageCategory::Action),
+                MessageDelay::None,
+            )
+        }
     }
 
     fn may_require_tick(&self) -> bool {
