232797ce297cb5b23ee4be4bce6716175ff63e8b
rotoclone <rotoclone@gmail.com>
2024-04-21 22:34:04 -0500

    Genericize message tokens type

--- crates/core-logic/src/message_format.rs
+++ crates/core-logic/src/message_format.rs
@@ -1,19 +1,23 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, marker::PhantomData};
 
 use bevy_ecs::prelude::*;
 
-pub struct MessageFormat(String);
+pub struct MessageFormat<T: MessageTokens>(String, PhantomData<fn(T)>);
 
-impl MessageFormat {
+pub trait MessageTokens {
+    fn get_tokens(&self) -> HashMap<String, Entity>;
+}
+
+impl<T: MessageTokens> MessageFormat<T> {
     /// Creates a `MessageFormat` with the provided format string.
     ///
     /// TODO explain format syntax
-    pub fn new(format_string: String) -> MessageFormat {
-        MessageFormat(format_string)
+    pub fn new(format_string: String) -> MessageFormat<T> {
+        MessageFormat(format_string, PhantomData)
     }
 
     /// Produces an interpolated string using the provided tokens.
-    pub fn interpolate(&self, tokens: HashMap<String, Entity>, world: &World) -> String {
+    pub fn interpolate(&self, tokens: T, world: &World) -> String {
         todo!() //TODO
     }
 }
