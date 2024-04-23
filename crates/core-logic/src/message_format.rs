0c964c3d71f49f43e99743ad90fbaff87c3cbb03
rotoclone <rotoclone@gmail.com>
2024-04-22 23:30:47 -0500

    Add documentation for format strings

--- crates/core-logic/src/message_format.rs
+++ crates/core-logic/src/message_format.rs
@@ -5,13 +5,27 @@ use bevy_ecs::prelude::*;
 pub struct MessageFormat<T: MessageTokens>(String, PhantomData<fn(T)>);
 
 pub trait MessageTokens {
-    fn get_tokens(&self) -> HashMap<String, Entity>;
+    fn get_token_map(&self) -> HashMap<String, Entity>;
 }
 
 impl<T: MessageTokens> MessageFormat<T> {
     /// Creates a `MessageFormat` with the provided format string.
     ///
-    /// TODO explain format syntax
+    /// Places for tokens in the format string are denoted by `%`.
+    /// Tokens can be in the following formats:
+    /// * `%name.type%`, where `name` is the name of the token, and `type` is one of the following types:
+    ///   * `name`: the entity's name
+    ///   * `they`: the entity's personal subject pronoun
+    ///   * `them`: the entity's personal object pronoun
+    ///   * `theirs`: the entity's possessive pronoun
+    ///   * `their`: the entity's possessive adjective pronoun
+    ///   * `themself`: the entity's reflexive pronoun
+    /// * `%name:a/b%`, where `name` is the name of the token, `a` is the text to use if the entity's pronouns are plural, and `b` is the text to use if the entity's pronouns are singular
+    ///
+    /// Token names must be alphanumeric.
+    ///
+    /// An example format string: `%attacker.name% throws %object.name%, but %target.name% moves out of the way just before %object.they% %object:hit/hits% %target.them%.`
+    /// This format string might produce the following result from `interpolate`: "Bob throws the rock, but Fred moves out of the way just before it hits him."
     pub fn new(format_string: String) -> MessageFormat<T> {
         MessageFormat(format_string, PhantomData)
     }
