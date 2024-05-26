293bfe96dc3cdc8d1e46305f58e6a4e4bceb8abc
rotoclone <rotoclone@gmail.com>
2024-05-26 12:28:40 -0500

    Fix baseball bat crit message

--- crates/core-logic/src/world_setup.rs
+++ crates/core-logic/src/world_setup.rs
@@ -753,7 +753,7 @@ pub fn spawn_start_building(
                 messages: WeaponMessages {
                     miss: vec![MessageFormat::new("${attacker.Name} ${attacker.you:swing/swings} ${weapon.name} wide of ${target.name}. Strike!").expect("message format should be valid")],
                     hit: vec![MessageFormat::new("${attacker.Name} ${attacker.you:bonk/bonks} ${target.name} on the ${body_part} with ${weapon.name}.").expect("message format should be valid")],
-                    crit: vec![MessageFormat::new("${attacker.Name} ${attacker.you:wind/winds} up with ${weapon.name} and ${attacker.connect/connects} with ${target.name's} ${body_part} with a loud crack.").expect("message format should be valid")],
+                    crit: vec![MessageFormat::new("${attacker.Name} ${attacker.you:wind/winds} up with ${weapon.name} and ${attacker.you:connect/connects} with ${target.name's} ${body_part} with a loud crack.").expect("message format should be valid")],
                 },
             },
             Volume(0.5),
