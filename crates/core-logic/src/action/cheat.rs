0bc5e6b2ce97e9b30be67324b75142cd6f0f6965
rotoclone <rotoclone@gmail.com>
2024-08-22 22:23:16 -0500

    Add set_hp cheat

--- crates/core-logic/src/action/cheat.rs
+++ crates/core-logic/src/action/cheat.rs
@@ -7,8 +7,10 @@ use crate::{
     component::{ActionEndNotification, AfterActionPerformNotification},
     input_parser::{CommandParseError, InputParseError, InputParser},
     notification::VerifyResult,
-    ActionTag, BeforeActionNotification, MessageCategory, MessageDelay, Notification,
-    VerifyActionNotification, World, Xp, XpAwardNotification,
+    vital_change::{ValueChangeOperation, VitalChangeMessageParams, VitalChangeVisualizationType},
+    ActionTag, BasicTokens, BeforeActionNotification, CommandTarget, Description, DynamicMessage,
+    MessageCategory, MessageDelay, MessageFormat, NoTokens, Notification, VerifyActionNotification,
+    VitalChange, VitalType, World, Xp, XpAwardNotification,
 };
 
 use super::{Action, ActionInterruptResult, ActionNotificationSender, ActionResult};
@@ -31,7 +33,14 @@ impl InputParser for CheatParser {
                     command: command_match.as_str().to_string(),
                     args: captures
                         .name(ARGS_CAPTURE)
-                        .map(|args_match| args_match.as_str().to_string()),
+                        .map(|args_match| {
+                            args_match
+                                .as_str()
+                                .split(" ")
+                                .map(|s| s.to_string())
+                                .collect::<Vec<String>>()
+                        })
+                        .unwrap_or_default(),
                     notification_sender: ActionNotificationSender::new(),
                 }));
             } else {
@@ -58,7 +67,7 @@ impl InputParser for CheatParser {
 #[derive(Debug)]
 pub struct CheatAction {
     pub command: String,
-    pub args: Option<String>,
+    pub args: Vec<String>,
     pub notification_sender: ActionNotificationSender<Self>,
 }
 
@@ -66,6 +75,7 @@ impl Action for CheatAction {
     fn perform(&mut self, performing_entity: Entity, world: &mut World) -> ActionResult {
         match self.command.as_str() {
             "give_xp" => give_xp(performing_entity, &self.args, world),
+            "set_hp" => set_hp(performing_entity, &self.args, world),
             x => ActionResult::error(performing_entity, format!("Unknown cheat command: {x}")),
         }
     }
@@ -115,9 +125,9 @@ impl Action for CheatAction {
     }
 }
 
-fn give_xp(entity: Entity, args: &Option<String>, world: &mut World) -> ActionResult {
-    if let Some(args) = args {
-        match args.parse() {
+fn give_xp(entity: Entity, args: &[String], world: &mut World) -> ActionResult {
+    if args.len() == 1 {
+        match args[0].parse() {
             Ok(amount) => {
                 Notification::send_no_contents(
                     XpAwardNotification {
@@ -138,6 +148,68 @@ fn give_xp(entity: Entity, args: &Option<String>, world: &mut World) -> ActionRe
             Err(e) => ActionResult::error(entity, format!("Error: {e}")),
         }
     } else {
-        ActionResult::error(entity, "give_xp requires 1 numeric argument".to_string())
+        ActionResult::error(entity, "give_xp requires 1 number".to_string())
+    }
+}
+
+fn set_hp(entity: Entity, args: &[String], world: &mut World) -> ActionResult {
+    let target;
+    let new_hp;
+    if args.len() == 1 {
+        target = entity;
+        new_hp = &args[0];
+    } else if args.len() == 2 {
+        let target_name = &args[0];
+        if let Some(t) = CommandTarget::parse(target_name).find_target_entity(entity, world) {
+            target = t;
+        } else {
+            return ActionResult::error(entity, format!("Invalid target name: {target_name}",));
+        }
+        new_hp = &args[1];
+    } else {
+        return ActionResult::error(
+            entity,
+            "set_hp requires 1 number or 1 target name and 1 number".to_string(),
+        );
+    };
+
+    match new_hp.parse() {
+        Ok(amount) => {
+            VitalChange::<NoTokens> {
+                entity: target,
+                vital_type: VitalType::Health,
+                operation: ValueChangeOperation::Set,
+                amount,
+                message_params: vec![(
+                    VitalChangeMessageParams::Direct {
+                        entity,
+                        message: "Zorp, HP magic".to_string(),
+                        category: MessageCategory::System,
+                    },
+                    VitalChangeVisualizationType::Full,
+                )],
+            }
+            .apply(world);
+
+            let message = MessageFormat::new("Set ${target.name's} HP to ${amount}.")
+                .expect("message format should be valid")
+                .interpolate(
+                    entity,
+                    &BasicTokens::new()
+                        .with_entity("target".into(), target)
+                        .with_string("amount".into(), amount.to_string()),
+                    world,
+                )
+                .expect("message should interpolate correctly");
+
+            ActionResult::message(
+                entity,
+                message,
+                MessageCategory::System,
+                MessageDelay::None,
+                false,
+            )
+        }
+        Err(e) => ActionResult::error(entity, format!("Error: {e}")),
     }
 }
