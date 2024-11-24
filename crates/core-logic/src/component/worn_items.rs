62bc6e79bb36bb8ce7246c8dcf198ffa2969c0ad
rotoclone <rotoclone@gmail.com>
2024-11-23 20:29:05 -0600

    Fix WornItems (doesn't compile)

--- crates/core-logic/src/component/worn_items.rs
+++ crates/core-logic/src/component/worn_items.rs
@@ -24,7 +24,7 @@ pub struct WornItems {
     /// (This is only relevant when trying to wear something on top of something else; a single wearable item can always be worn regardless of its thickness.)
     pub max_thickness: u32,
     /// Map of body parts to the items being worn on them.
-    items: HashMap<Entity, Vec<Entity>>,
+    body_part_to_items: HashMap<Entity, Vec<Entity>>,
 }
 
 /// An error when trying to wear something.
@@ -37,7 +37,10 @@ pub enum WearError {
     /// The entity is already wearing the item.
     AlreadyWorn,
     /// The item is too thick for a body part due to another item on it.
-    TooThick(BodyPart, Entity),
+    TooThick {
+        body_part: Entity,
+        worn_item: Entity,
+    },
 }
 
 /// An error when trying to take something off.
@@ -51,18 +54,22 @@ impl WornItems {
     pub fn new(max_thickness: u32) -> WornItems {
         WornItems {
             max_thickness,
-            items: HashMap::new(),
+            body_part_to_items: HashMap::new(),
         }
     }
 
     /// Gets all the entities being worn.
     pub fn get_all_items(&self) -> HashSet<Entity> {
-        self.items.values().flatten().cloned().collect()
+        self.body_part_to_items
+            .values()
+            .flatten()
+            .cloned()
+            .collect()
     }
 
     /// Determines whether the provided entity is being worn.
     pub fn is_wearing(&self, entity: Entity) -> bool {
-        for worn_items in &mut self.items.values() {
+        for worn_items in &mut self.body_part_to_items.values() {
             if worn_items.contains(&entity) {
                 return true;
             }
@@ -96,33 +103,48 @@ impl WornItems {
             None => return Err(WearError::NotWearable),
         };
 
-        for body_part in &wearable.body_parts {
-            if let Some(already_worn) = self.items.get(body_part) {
-                if already_worn.contains(&entity) {
-                    return Err(WearError::AlreadyWorn);
-                }
+        if self.is_wearing(entity) {
+            return Err(WearError::AlreadyWorn);
+        }
 
-                // check total thickness, but only if there's already at least one thing on this body part
-                // TODO do this in a validate notification handler for the wear action instead?
-                if !already_worn.is_empty() {
-                    let total_thickness = already_worn
-                        .iter()
-                        .map(|e| get_thickness(*e, world))
-                        .sum::<u32>();
-                    if total_thickness + wearable.thickness > self.max_thickness {
-                        return Err(WearError::TooThick(
-                            body_part.clone(),
-                            // unwrap is safe because we've already checked if `already_worn` is empty
-                            *already_worn.last().unwrap(),
-                        ));
+        let mut body_parts_to_wear_on = HashSet::new();
+        for body_part_type in &wearable.body_parts {
+            let body_parts = BodyPart::get(body_part_type, entity, world);
+            let mut peekable_body_parts = body_parts.iter().peekable();
+            'body_parts_of_type: while let Some((body_part_entity, _)) = peekable_body_parts.next()
+            {
+                if let Some(already_worn) = self.body_part_to_items.get(body_part_entity) {
+                    // check total thickness, but only if there's already at least one thing on this body part
+                    if !already_worn.is_empty() {
+                        let total_thickness = already_worn
+                            .iter()
+                            .map(|e| get_thickness(*e, world))
+                            .sum::<u32>();
+                        if total_thickness + wearable.thickness > self.max_thickness {
+                            // if the wearing entity has another body part of this type, maybe it can go on that one
+                            if peekable_body_parts.peek().is_some() {
+                                continue 'body_parts_of_type;
+                            }
+
+                            return Err(WearError::TooThick {
+                                body_part: *body_part_entity,
+                                // unwrap is safe because we've already checked if `already_worn` is empty
+                                worn_item: *already_worn.last().unwrap(),
+                            });
+                        }
                     }
                 }
+
+                // if we've gotten to this point, then the entity can be worn on this body part
+                body_parts_to_wear_on.insert(*body_part_entity);
+                // stop looping through body parts of this type, since a valid one was found
+                break 'body_parts_of_type;
             }
         }
 
-        for body_part in &wearable.body_parts {
-            self.items
-                .entry(body_part.clone())
+        for body_part in body_parts_to_wear_on {
+            self.body_part_to_items
+                .entry(body_part)
                 .or_default()
                 .push(entity);
         }
@@ -143,7 +165,7 @@ impl WornItems {
         };
 
         let mut removed = false;
-        for items in worn_items.items.values_mut() {
+        for items in worn_items.body_part_to_items.values_mut() {
             if items.contains(&to_remove) {
                 items.retain(|e| *e != to_remove);
                 removed = true;
@@ -182,7 +204,7 @@ impl AttributeDescriber for WornItemsAttributeDescriber {
         let mut descriptions = Vec::new();
         if let Some(worn_items) = world.get::<WornItems>(entity) {
             let worn_entity_names = worn_items
-                .items
+                .body_part_to_items
                 .values()
                 .flat_map(|items| items.last())
                 .unique()
