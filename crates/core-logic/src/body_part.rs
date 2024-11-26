2af687e0a251fbeb3e451a79bb7d3ff876348c9a
rotoclone <rotoclone@gmail.com>
2024-11-25 23:46:47 -0600

    Change how body parts are stored (doesn't compile)

--- crates/core-logic/src/body_part.rs
+++ crates/core-logic/src/body_part.rs
@@ -1,13 +1,24 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use bevy_ecs::prelude::*;
 
 use rand::seq::SliceRandom;
 use strum::EnumIter;
 
-/// The body parts an entity has.
+/// The body parts an entity has, grouped by type.
 #[derive(Component)]
-pub struct BodyParts(pub HashSet<Entity>);
+pub struct BodyParts(HashMap<BodyPartType, Vec<Entity>>);
+
+/// An error returned when an entity is not a body part.
+pub struct NotABodyPartError(pub Entity);
+
+impl BodyParts {
+    /// Creates a set of body parts from the provided body part entities.
+    /// If any of the provided entities are not body parts, `Err(NotABodyPartError)` will be returned.
+    pub fn new(parts: &[Entity], world: &World) -> Result<BodyParts, NotABodyPartError> {
+        todo!() //TODO
+    }
+}
 
 /// A single body part of an entity.
 #[derive(Debug, Clone, Component)]
@@ -38,33 +49,25 @@ pub struct BodyPartDamageMultiplier(pub f32);
 
 impl BodyPart {
     /// Gets all the body parts of a type from the provided entity.
-    pub fn get<'w>(
-        body_part_type: &BodyPartType,
-        entity: Entity,
-        world: &'w World,
-    ) -> Vec<(Entity, &'w BodyPart)> {
-        todo!() //TODO
+    pub fn get<'w>(body_part_type: &BodyPartType, entity: Entity, world: &'w World) -> Vec<Entity> {
+        world
+            .get::<BodyParts>(entity)
+            .and_then(|parts| parts.0.get(body_part_type).cloned())
+            .unwrap_or_default()
     }
 
     /// Gets a random body part from the provided entity, if it has any body parts.
-    pub fn random_weighted(entity: Entity, world: &World) -> Option<(Entity, &BodyPart)> {
+    pub fn random_weighted(entity: Entity, world: &World) -> Option<Entity> {
         let mut rng = rand::thread_rng();
         world
             .get::<BodyParts>(entity)
             //TODO allow different weights
             .map(|b| {
-                b.0.iter()
-                    .map(|e| {
-                        (
-                            *e,
-                            world
-                                .get::<BodyPart>(*e)
-                                .expect("body part should be a body part"),
-                        )
-                    })
-                    .collect::<Vec<(Entity, &BodyPart)>>()
+                b.0.values()
+                    .flatten()
+                    .collect::<Vec<&Entity>>()
                     .choose(&mut rng)
-                    .map(|tuple| *tuple)
+                    .map(|e| **e)
             })
             .unwrap_or_default()
     }
