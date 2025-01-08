43ee9c69eaa12bf2803f2cee1e73713f3e085d41
rotoclone <rotoclone@gmail.com>
2025-01-07 22:44:13 -0600

    Allow preceding entity names with 'the'

--- crates/core-logic/src/command_format/part_parsers/entity_parser.rs
+++ crates/core-logic/src/command_format/part_parsers/entity_parser.rs
@@ -1,6 +1,7 @@
 use std::any::Any;
 
 use bevy_ecs::prelude::*;
+use nom::{bytes::complete::tag, combinator::opt, sequence::preceded, IResult, Parser};
 
 use crate::{find_entities_in_presence_of, Description};
 
@@ -16,14 +17,12 @@ impl ParsePart<Entity> for EntityParser {
     fn parse(&self, context: PartParserContext, world: &World) -> CommandPartParseResult<Entity> {
         for entity in find_entities_in_presence_of(context.entering_entity, world) {
             for name in Description::get_all_ways_to_reference(entity, world) {
-                if let Ok((remaining, _)) = match_literal_ignore_case(name, context.input.as_str())
-                {
+                if let Ok((remaining, _)) = match_entity_name(name, context.input.as_str()) {
                     return CommandPartParseResult::Success {
                         parsed: entity,
                         remaining: remaining.to_string(),
                     };
                 }
-                //TODO allow putting "the" before the entity's name
                 //TODO return an error if there are multiple matching entities?
             }
         }
@@ -47,4 +46,9 @@ impl ParsePartUntyped for EntityParser {
     ) -> CommandPartParseResult<Box<dyn Any>> {
         self.parse(context, world).into_generic()
     }
+}
+
+/// Matches the name of an entity, optionally preceded by "the".
+fn match_entity_name<'i>(name: &str, input: &'i str) -> IResult<&'i str, &'i str> {
+    preceded(opt(tag("the ")), |i| match_literal_ignore_case(name, i))(input)
 }
