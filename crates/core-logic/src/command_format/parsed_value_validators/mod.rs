26eaf779b953ed0c8997477090137f3c78af93a7
rotoclone <rotoclone@gmail.com>
2026-02-05 23:40:11 -0600

    Remove a bunch of commented-out code

--- crates/core-logic/src/command_format/parsed_value_validators/mod.rs
+++ crates/core-logic/src/command_format/parsed_value_validators/mod.rs
@@ -2,52 +2,6 @@ use bevy_ecs::prelude::*;
 
 use crate::component::Description;
 
-/* TODO remove
-pub trait ValidateParsedValue<T>: ValidateParsedValueUntyped + ValidateParsedValueClone<T> {
-    fn validate(
-        &self,
-        context: PartValidatorContext<T>,
-        world: &World,
-    ) -> CommandPartValidateResult;
-
-    fn as_untyped(&self) -> Box<dyn ValidateParsedValueUntyped>;
-}
-
-pub trait ValidateParsedValueUntyped:
-    std::fmt::Debug + Send + Sync + ValidateParsedValueUntypedClone
-{
-    fn validate(
-        &self,
-        context: PartValidatorContext<ParsedValue>,
-        world: &World,
-    ) -> CommandPartValidateResult;
-}
-
-/// This trait exists because adding regular `Clone` to a trait makes it not object-safe, but doing this silly thing works apparently.
-/// https://stackoverflow.com/a/30353928
-pub trait ValidateParsedValueUntypedClone {
-    fn clone_box(&self) -> Box<dyn ValidateParsedValueUntyped>;
-}
-
-impl<T: 'static + ValidateParsedValueUntyped + Clone> ValidateParsedValueUntypedClone for T {
-    fn clone_box(&self) -> Box<dyn ValidateParsedValueUntyped> {
-        Box::new(self.clone())
-    }
-}
-
-/// This trait exists because adding regular `Clone` to a trait makes it not object-safe, but doing this silly thing works apparently.
-/// https://stackoverflow.com/a/30353928
-pub trait ValidateParsedValueClone<T> {
-    fn clone_box(&self) -> Box<dyn ValidateParsedValue<T>>;
-}
-
-impl<T: 'static + ValidateParsedValue<P> + Clone, P> ValidateParsedValueClone<P> for T {
-    fn clone_box(&self) -> Box<dyn ValidateParsedValue<P>> {
-        Box::new(self.clone())
-    }
-}
-    */
-
 /// TODO doc
 #[derive(Debug)]
 pub struct PartValidatorContext<T> {
