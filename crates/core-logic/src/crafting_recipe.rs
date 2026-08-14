82c6572414bcfe9e095ff9383663b42eb7a68346
rotoclone <rotoclone@gmail.com>
2026-08-13 20:53:02 -0500

    Try to figure out how to check entities for tags

--- crates/core-logic/src/crafting_recipe.rs
+++ crates/core-logic/src/crafting_recipe.rs
@@ -1,4 +1,8 @@
-use std::collections::HashSet;
+use std::{
+    any::Any,
+    collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
+};
 
 use bevy_ecs::prelude::*;
 
@@ -6,14 +10,14 @@ use crate::{Volume, Weight};
 
 pub struct CraftingRecipe {
     name: String,
-    components: Vec<CraftingRecipeComponent>,
+    ingredients: Vec<CraftingRecipeIngredient>,
 }
 
-pub struct CraftingRecipeComponent {
+pub struct CraftingRecipeIngredient {
     name: String,
-    //TODO allow grouping components, i.e. "you have to provide at least one of these but not all of them"
+    //TODO allow grouping ingredients, i.e. "you have to provide at least one of these but not all of them"
     required: bool,
-    amount: CraftingComponentAmount,
+    amount: CraftingIngredientAmount,
     tags: Vec<TagBounds>,
 }
 
@@ -24,14 +28,21 @@ pub enum TagBounds {
     NotAnyOf(HashSet<TagDescriptor>),
 }
 
-#[derive(PartialEq, Eq, Hash)]
-pub enum TagDescriptor {
-    Category(TagCategory),
-    Tag(CraftingTagId),
+impl TagBounds {
+    /// Determines whether the provided entity is within these bounds.
+    fn matches(&self, entity: Entity, world: &World) -> bool {
+        todo!() //TODO
+    }
 }
 
-/// The amount of a component needed for a crafting recipe.
-pub enum CraftingComponentAmount {
+#[derive(Hash)]
+pub enum TagDescriptor {
+    Category(TagCategory),
+    Tag(Box<dyn CraftingTag>),
+}
+
+/// The amount of an ingredient needed for a crafting recipe.
+pub enum CraftingIngredientAmount {
     /// A number of items (e.g. 3 screws)
     Items(usize),
     /// A mass of material (e.g. 3 kg of metal)
@@ -52,17 +63,60 @@ pub enum TagCategory {
     Custom(String),
 }
 
+pub struct CraftingTags(HashSet<Box<dyn CraftingTag>>);
+
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct CraftingTagId(&'static str);
 
 //TODO this should also go isomewhere else
-pub trait CraftingTag: Component {
-    /// Gets the ID of this tag
-    fn id() -> CraftingTagId;
+pub trait CraftingTag {
+    /// Determines whether this tag is equal to the provided one, by type
+    fn equals(&self, other: &dyn CraftingTag) -> bool;
 
-    /// Gets the category of this tag
-    /// TODO should this return an `Option` to support category-less tags, like tagging something as an adhesive?
-    fn category() -> TagCategory;
+    /// Gets the ID of this tag
+    fn id(&self) -> CraftingTagId;
+
+    /// Gets the category of this tag, if it has one
+    fn category(&self) -> Option<TagCategory>;
+}
+
+impl Hash for dyn CraftingTag {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.dyn_hash(state);
+    }
+}
+impl PartialEq for dyn CraftingTag {
+    fn eq(&self, other: &dyn CraftingTag) -> bool {
+        TypeEq::type_eq(self, other.as_any())
+    }
+}
+impl Eq for dyn CraftingTag {}
+
+trait AsAny {
+    fn as_any(&self) -> &dyn Any;
+}
+impl<T: Any> AsAny for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+trait DynHash {
+    fn dyn_hash(&self, state: &mut dyn Hasher);
+}
+impl<H: Hash + ?Sized> DynHash for H {
+    fn dyn_hash(&self, mut state: &mut dyn Hasher) {
+        self.hash(&mut state);
+    }
+}
+
+trait TypeEq {
+    fn type_eq(&self, other: &dyn Any) -> bool;
+}
+impl<T: Any> TypeEq for T {
+    fn type_eq(&self, other: &dyn Any) -> bool {
+        other.downcast_ref::<Self>().is_some()
+    }
 }
 
 //TODO these tag structs should go somewhere else
@@ -79,8 +133,8 @@ impl CraftingTag for SizeSmall {
         SIZE_SMALL_ID
     }
 
-    fn category() -> TagCategory {
-        TagCategory::Size
+    fn category() -> Option<TagCategory> {
+        Some(TagCategory::Size)
     }
 }
 
@@ -92,8 +146,8 @@ impl CraftingTag for SizeMedium {
         SIZE_MEDIUM_ID
     }
 
-    fn category() -> TagCategory {
-        TagCategory::Size
+    fn category() -> Option<TagCategory> {
+        Some(TagCategory::Size)
     }
 }
 
@@ -105,8 +159,8 @@ impl CraftingTag for SizeLarge {
         SIZE_LARGE_ID
     }
 
-    fn category() -> TagCategory {
-        TagCategory::Size
+    fn category() -> Option<TagCategory> {
+        Some(TagCategory::Size)
     }
 }
 
@@ -121,8 +175,8 @@ impl CraftingTag for ShapeRod {
         SHAPE_ROD_ID
     }
 
-    fn category() -> TagCategory {
-        TagCategory::Shape
+    fn category() -> Option<TagCategory> {
+        Some(TagCategory::Shape)
     }
 }
 
@@ -134,8 +188,8 @@ impl CraftingTag for ShapeRope {
         SHAPE_ROPE_ID
     }
 
-    fn category() -> TagCategory {
-        TagCategory::Shape
+    fn category() -> Option<TagCategory> {
+        Some(TagCategory::Shape)
     }
 }
 
@@ -143,29 +197,29 @@ impl CraftingTag for ShapeRope {
 fn build_test_recipe() -> CraftingRecipe {
     CraftingRecipe {
         name: "Axe".to_string(),
-        components: vec![
-            CraftingRecipeComponent {
+        ingredients: vec![
+            CraftingRecipeIngredient {
                 name: "handle".to_string(),
                 required: true,
-                amount: CraftingComponentAmount::Items(1),
+                amount: CraftingIngredientAmount::Items(1),
                 tags: vec![
                     TagBounds::Just(TagDescriptor::Tag(SizeMedium::id())),
                     TagBounds::Just(TagDescriptor::Tag(ShapeRod::id())),
                 ],
             },
-            CraftingRecipeComponent {
+            CraftingRecipeIngredient {
                 name: "head".to_string(),
                 required: true,
-                amount: CraftingComponentAmount::Items(1),
+                amount: CraftingIngredientAmount::Items(1),
                 tags: vec![
                     TagBounds::Just(TagDescriptor::Tag(SizeMedium::id())),
                     TagBounds::Just(TagDescriptor::Category(TagCategory::Sharpness)),
                 ],
             },
-            CraftingRecipeComponent {
+            CraftingRecipeIngredient {
                 name: "rope".to_string(),
                 required: true,
-                amount: CraftingComponentAmount::Items(1),
+                amount: CraftingIngredientAmount::Items(1),
                 tags: vec![
                     TagBounds::Just(TagDescriptor::Tag(SizeSmall::id())),
                     TagBounds::Just(TagDescriptor::Tag(ShapeRope::id())),
@@ -173,4 +227,15 @@ fn build_test_recipe() -> CraftingRecipe {
             },
         ],
     }
+}
+
+fn entity_valid_for_ingredient(
+    entity: Entity,
+    ingredient: CraftingRecipeIngredient,
+    world: &World,
+) -> bool {
+    ingredient
+        .tags
+        .iter()
+        .all(|tag_bounds| tag_bounds.matches(entity, world))
 }
