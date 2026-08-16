58dbb134c71efe82dbaf93273152bc4803f3e9e0
rotoclone <rotoclone@gmail.com>
2026-08-16 13:40:54 -0500

    Remove silly IDs and base equality on the types themselves

--- crates/core-logic/src/crafting_recipe.rs
+++ crates/core-logic/src/crafting_recipe.rs
@@ -1,5 +1,5 @@
 use std::{
-    any::Any,
+    any::{Any, TypeId},
     collections::{HashMap, HashSet},
     hash::{Hash, Hasher},
 };
@@ -41,6 +41,13 @@ pub enum TagDescriptor {
     Tag(Box<dyn CraftingTag>),
 }
 
+impl TagDescriptor {
+    /// Determines whether the provided entity has a tag matching this descriptor
+    fn matches(&self, entity: Entity, world: &World) -> bool {
+        todo!() //TODO
+    }
+}
+
 /// The amount of an ingredient needed for a crafting recipe.
 pub enum CraftingIngredientAmount {
     /// A number of items (e.g. 3 screws)
@@ -63,19 +70,16 @@ pub enum TagCategory {
     Custom(String),
 }
 
+#[derive(Component)]
 pub struct CraftingTags(HashSet<Box<dyn CraftingTag>>);
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct CraftingTagId(&'static str);
 
 //TODO this should also go isomewhere else
-pub trait CraftingTag {
-    /// Determines whether this tag is equal to the provided one, by type
-    fn equals(&self, other: &dyn CraftingTag) -> bool;
-
-    /// Gets the ID of this tag
-    fn id(&self) -> CraftingTagId;
-
+/// Trait for crafting tags.
+/// Equality is based solely on types, so crafting tag structs should not have any fields.
+pub trait CraftingTag: AsAny + Send + Sync {
     /// Gets the category of this tag, if it has one
     fn category(&self) -> Option<TagCategory>;
 }
@@ -87,12 +91,12 @@ impl Hash for dyn CraftingTag {
 }
 impl PartialEq for dyn CraftingTag {
     fn eq(&self, other: &dyn CraftingTag) -> bool {
-        TypeEq::type_eq(self, other.as_any())
+        self.as_any().type_id() == other.as_any().type_id()
     }
 }
 impl Eq for dyn CraftingTag {}
 
-trait AsAny {
+pub trait AsAny {
     fn as_any(&self) -> &dyn Any;
 }
 impl<T: Any> AsAny for T {
@@ -110,90 +114,51 @@ impl<H: Hash + ?Sized> DynHash for H {
     }
 }
 
-trait TypeEq {
-    fn type_eq(&self, other: &dyn Any) -> bool;
-}
-impl<T: Any> TypeEq for T {
-    fn type_eq(&self, other: &dyn Any) -> bool {
-        other.downcast_ref::<Self>().is_some()
-    }
-}
-
 //TODO these tag structs should go somewhere else
-static SIZE_SMALL_ID: CraftingTagId = CraftingTagId("size_small");
-static SIZE_MEDIUM_ID: CraftingTagId = CraftingTagId("size_medium");
-static SIZE_LARGE_ID: CraftingTagId = CraftingTagId("size_large");
 
-#[derive(Component)]
 pub struct SizeSmall;
 
 //TODO make a proc macro to auto-derive this
 impl CraftingTag for SizeSmall {
-    fn id() -> CraftingTagId {
-        SIZE_SMALL_ID
-    }
-
-    fn category() -> Option<TagCategory> {
+    fn category(&self) -> Option<TagCategory> {
         Some(TagCategory::Size)
     }
 }
 
-#[derive(Component)]
 pub struct SizeMedium;
 
 impl CraftingTag for SizeMedium {
-    fn id() -> CraftingTagId {
-        SIZE_MEDIUM_ID
-    }
-
-    fn category() -> Option<TagCategory> {
+    fn category(&self) -> Option<TagCategory> {
         Some(TagCategory::Size)
     }
 }
 
-#[derive(Component)]
 pub struct SizeLarge;
 
 impl CraftingTag for SizeLarge {
-    fn id() -> CraftingTagId {
-        SIZE_LARGE_ID
-    }
-
-    fn category() -> Option<TagCategory> {
+    fn category(&self) -> Option<TagCategory> {
         Some(TagCategory::Size)
     }
 }
 
-static SHAPE_ROD_ID: CraftingTagId = CraftingTagId("shape_rod");
-static SHAPE_ROPE_ID: CraftingTagId = CraftingTagId("shape_rope");
-
-#[derive(Component)]
 pub struct ShapeRod;
 
 impl CraftingTag for ShapeRod {
-    fn id() -> CraftingTagId {
-        SHAPE_ROD_ID
-    }
-
-    fn category() -> Option<TagCategory> {
+    fn category(&self) -> Option<TagCategory> {
         Some(TagCategory::Shape)
     }
 }
 
-#[derive(Component)]
 pub struct ShapeRope;
 
 impl CraftingTag for ShapeRope {
-    fn id() -> CraftingTagId {
-        SHAPE_ROPE_ID
-    }
-
-    fn category() -> Option<TagCategory> {
+    fn category(&self) -> Option<TagCategory> {
         Some(TagCategory::Shape)
     }
 }
 
 //TODO remove
+#[allow(unused)]
 fn build_test_recipe() -> CraftingRecipe {
     CraftingRecipe {
         name: "Axe".to_string(),
@@ -203,8 +168,8 @@ fn build_test_recipe() -> CraftingRecipe {
                 required: true,
                 amount: CraftingIngredientAmount::Items(1),
                 tags: vec![
-                    TagBounds::Just(TagDescriptor::Tag(SizeMedium::id())),
-                    TagBounds::Just(TagDescriptor::Tag(ShapeRod::id())),
+                    TagBounds::Just(TagDescriptor::Tag(Box::new(SizeMedium))),
+                    TagBounds::Just(TagDescriptor::Tag(Box::new(ShapeRod))),
                 ],
             },
             CraftingRecipeIngredient {
@@ -212,7 +177,7 @@ fn build_test_recipe() -> CraftingRecipe {
                 required: true,
                 amount: CraftingIngredientAmount::Items(1),
                 tags: vec![
-                    TagBounds::Just(TagDescriptor::Tag(SizeMedium::id())),
+                    TagBounds::Just(TagDescriptor::Tag(Box::new(SizeMedium))),
                     TagBounds::Just(TagDescriptor::Category(TagCategory::Sharpness)),
                 ],
             },
@@ -221,8 +186,8 @@ fn build_test_recipe() -> CraftingRecipe {
                 required: true,
                 amount: CraftingIngredientAmount::Items(1),
                 tags: vec![
-                    TagBounds::Just(TagDescriptor::Tag(SizeSmall::id())),
-                    TagBounds::Just(TagDescriptor::Tag(ShapeRope::id())),
+                    TagBounds::Just(TagDescriptor::Tag(Box::new(SizeSmall))),
+                    TagBounds::Just(TagDescriptor::Tag(Box::new(ShapeRope))),
                 ],
             },
         ],
